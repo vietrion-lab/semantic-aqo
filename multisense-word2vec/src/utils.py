f22fe3726211fe72655c7cc2e1646325d787f3fc
hqvjet <viethq.1906@gmail.com>
2026-01-20 16:16:08 +0700

    feat: increase lr, batch

--- multisense-word2vec/src/utils.py
+++ multisense-word2vec/src/utils.py
@@ -37,7 +37,7 @@ def load_data(owner: str, dataset_name: str) -> list:
     # Setup auth first
     setup_huggingface_auth()
     dataset = load_dataset(f"{owner}/{dataset_name}")['train']
-    return dataset['statement'][:15] # Restrict the records number due to GPU limitation
+    return dataset['statement'][:5000] # Restrict the records number due to GPU limitation
 
 def load_config(path: str) -> GlobalConfigSchema:
     with open(path, 'r') as file:
